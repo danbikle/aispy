21a237f58f8f04165130e4cf38c61c6f60cad15e
Dan Bikle <dan.bikle@gmail.com>
2015-04-06 18:12:27 -0700

    enhancements

--- sparkling-water-droplet/src/main/scala/water/droplets/AiSpy.scala
+++ sparkling-water-droplet/src/main/scala/water/droplets/AiSpy.scala
@@ -22,51 +22,18 @@ object AiSpy {
     import h2oContext._
 
     // Register file to be available on all nodes
-    sc.addFile("data/iris.csv")
+    sc.addFile("data/ftr_ff_GSPC_train.csv")
+    sc.addFile("data/ftr_ff_GSPC_oos.csv"  )
 
     // Load data and parse it via h2o parser
-    val irisTable = new DataFrame(new File(SparkFiles.get("iris.csv")))
-
-    // Build GBM model
-    val gbmParams = new GBMParameters()
-    gbmParams._train = irisTable
-    gbmParams._response_column = 'class
-    gbmParams._ntrees = 5
-
-    val gbm = new GBM(gbmParams)
-    val gbmModel = gbm.trainModel.get
-
-    // Make prediction on train data
-    val predict = gbmModel.score(irisTable)('predict)
-
-    // Compute number of mispredictions with help of Spark API
-    val trainRDD = asRDD[StringHolder](irisTable('class))
-    val predictRDD = asRDD[StringHolder](predict)
-
-    // Make sure that both RDDs has the same number of elements
-    assert(trainRDD.count() == predictRDD.count)
-    val numMispredictions = trainRDD.zip(predictRDD).filter( i => {
-      val act = i._1
-      val pred = i._2
-      act.result != pred.result
-    }).collect()
-
-    println(
-      s"""
-         |Number of mispredictions: ${numMispredictions.length}
-         |
-         |Mispredictions:
-         |
-         |actual X predicted
-         |------------------
-         |${numMispredictions.map(i => i._1.result.get + " X " + i._2.result.get).mkString("\n")}
-       """.stripMargin)
+    val train_df = new DataFrame(new File(SparkFiles.get("ftr_ff_GSPC_train.csv")))
+    val oos_df   = new DataFrame(new File(SparkFiles.get("ftr_ff_GSPC_oos.csv"  )))
 
     // Shutdown application
     sc.stop()
   }
 
-  def configure(appName:String = "Sparkling Water Demo"):SparkConf = {
+  def configure(appName:String = "AiSpy Demo"):SparkConf = {
     val conf = new SparkConf()
       .setAppName(appName)
     conf.setIfMissing("spark.master", sys.env.getOrElse("spark.master", "local"))
